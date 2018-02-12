8f377b9937c2bbcfdfc0c720fee84e10c892073c
pabloonieva <pablo@ironhack.com>
2018-02-12 09:31:18 +0100

    feat: add methods to user.model.js

--- starter-code/models/user.model.js
+++ starter-code/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const HASH_FACTOR = 10;
 
 //creating user class or constructor
 // we have name, email, password, address and paymentMethod
@@ -21,4 +23,31 @@ const userSchema = new mongoose.Schema({
   paymentMethod: {
     type: Number
   }
+}, { timestamps: true });
+
+//We need methodes
+userSchema.pre('save', function(next) {    //function (next)¿?
+    const user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.genSalt(HASH_FACTOR)
+        .then(salt => {
+            bcrypt.hash(user.password, salt)
+                .then(hash => {
+                    user.password = hash;
+                    next();
+                });
+        })
+        .catch(error => next(error));
 });
+
+userSchema.methods.checkPassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+
+//We export the schemas
+const User = mongoose.model('User', userSchema);  //¿?
+module.exports = User;
