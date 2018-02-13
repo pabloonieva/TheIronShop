975656ce852c0bc300ac019ddaf44b2ff503f6aa
pabloonieva <pablo@ironhack.com>
2018-02-13 21:08:29 +0100

    feat: add controllers without functions and comment routes of products and shoppingCart

---
+++ starter-code/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+module.exports.signup = (req, res, next) => {
+    //we must add here the function
+};
+
+module.exports.doSignup = (req, res, next) => {
+    //we must add here the function
+};
+module.exports.login = (req, res, next) => {
+    //we must add here the function
+};
+module.exports.doLogin = (req, res, next) => {
+    //we must add here the function
+};
+module.exports.logout = (req, res, next) => {
+    //we must add here the function
+};
--- starter-code/routes/shoppingCart.routes.js
+++ starter-code/routes/shoppingCart.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../controllers/shoppingCart.controller');
 
+/*
 router.get('/shoppingCart', shoppingCartController.showUser);
 router.post('/shoppingCart', shoppingCartController.buyProducts);
+*/
 
-+module.exports = router;
+module.exports = router;
