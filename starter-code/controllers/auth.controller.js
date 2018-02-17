fe1cc0ba2e619976f308dfb018531e2185ce3764
pabloonieva <pablo@ironhack.com>
2018-02-17 12:53:21 +0100

    feat: add config passport

--- starter-code/controllers/auth.controller.js
+++ starter-code/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const User = require('../models/user.model');
 
 module.exports.signup = (req, res, next) => {
     //we must add here the function
@@ -8,10 +9,30 @@ module.exports.doSignup = (req, res, next) => {
     //we must add here the function
 };
 module.exports.login = (req, res, next) => {
-    res.render();
+    res.render('auth/login',{
+        flash: req.flash()
+    });
 };
 module.exports.doLogin = (req, res, next) => {
-    //we must add here the function
+    const email = req.body.email;
+    const password = req.body.password;
+    if (!email || !password){
+      res.render('auth/login',{
+        email: { email: email },
+        error: {
+                  email: email ? '' : 'Email is required',
+                  password: password ? '' : 'Password is required'
+              }
+      });
+    } else {
+      User.findOne({email: email})
+        .then(result => {
+
+        })
+        .catch(error => {
+
+        });
+    }
 };
 module.exports.logout = (req, res, next) => {
     //we must add here the function
